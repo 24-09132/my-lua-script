23a7e5cb57ce028336a1a4a286512196007a408f
24-09132 <24-09132@g.batstate-u.edu.ph>
2026-02-23 15:13:03 +0800

    Update script URL to use refs/heads/main

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local scriptCode = game:HttpGet(
-"https://raw.githubusercontent.com/24-09132/my-lua-script/main/main.lua"
+"https://raw.githubusercontent.com/24-09132/my-lua-script/refs/heads/main/main.lua"
 )
 
 loadstring(scriptCode)()
